c3af65c51b265afe62c91bada4edc4170bdc03da
PranavBalu <51266456+pranavbalu1@users.noreply.github.com>
2023-01-28 13:03:15 -0600

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastai import *
+from fastai.vision import *
+from fastai.metrics import error_rate
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
