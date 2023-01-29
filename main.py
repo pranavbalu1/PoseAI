efe625b90ef606b4adad307af470b76656f9374d
PranavBalu <51266456+pranavbalu1@users.noreply.github.com>
2023-01-28 18:51:57 -0600

    add tf and keras

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import tensorflow as tf
+import keras as keras
 from keras.preprocessing.image import ImageDataGenerator
-import keras
+
 
 
 yogadataset = "./yogadataset"
